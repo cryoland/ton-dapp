7d40dfbacd571dae5e6ef1f9f66a076a80f2fa04
ValenTiN <vsrknv@gmail.com>
2024-05-14 00:31:51 +0300

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { App } from './App';
 
-// this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/cryoland/ton-learn/master/ton-course/dapp/manifest.json';
-
+const manifestUrl = 'https://cryoland.github.io/ton-dapp/manifest.json';
 const root = createRoot(document.getElementById('app') as HTMLElement);
 
 root.render(
