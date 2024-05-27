a6c53ee53b55598ece660b14be1f6dc14ce2fad3
ValenTiN <vsrknv@gmail.com>
2024-05-28 00:28:56 +0300

    experiments with glitch

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,40 @@
 import { createRoot } from 'react-dom/client';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import { App } from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import { Glitch } from './Glitch';
 
 const manifestUrl = 'https://cryoland.github.io/ton-dapp/manifest.json';
 const root = createRoot(document.getElementById('app') as HTMLElement);
 
+const router = createBrowserRouter([
+  {
+    path: "/ton-dapp",
+    element: (
+      <TonConnectUIProvider manifestUrl={manifestUrl}>
+        <App />
+      </TonConnectUIProvider>
+    ),
+  },
+  {
+    path: "/ton-dapp/almost-here",
+    element: <Glitch image='almost-here.png' />,
+  },
+  {
+    path: "/ton-dapp/is-coming",
+    element: <Glitch image='is-coming.png' />,
+  },
+  {
+    path: "/ton-dapp/knock",
+    element: <Glitch image='knock.png' />,
+  },
+]);
+
 root.render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
-    <App />
-  </TonConnectUIProvider>
+  <RouterProvider router={router} />
 );
